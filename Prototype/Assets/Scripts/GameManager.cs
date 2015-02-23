55a409a48881295f190b589e243ccf1b8a235c4b
akaleth <emills441@gmail.com>
2015-02-23 16:39:20 -0500

    Initial prototype commit
    Changed some Unity project settings to make it work nicely with git

---
+++ Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    public PlayerSamurai Player1;
+    public PlayerSamurai Player2;
+    public PlayerSamurai Player3;
+    public PlayerSamurai Player4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Prototype/Assets/Scripts/PlayerSamurai.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSamurai : MonoBehaviour
+{
+
+    public int Might;
+    public int Honor;
+    public int Glory;
+    public int Cunning;
+    public int Wisdom;
+
+    public int Health;
+
+    public enum BodyState
+    {
+        Attacking, Running, Dashing, Idle
+    }
+
+    protected BodyState CurrentBodyState;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    Health = 2;
+	    Might = Honor = Glory = Cunning = Wisdom = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    
+
+	}
+
+    void Attack()
+    {
+        
+    }
+
+    void Dash()
+    {
+        
+    }
+
+    void Interact()
+    {
+        
+    }
+}
