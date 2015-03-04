d4d76fa2ed00fe542095cc767442c3730e9fcf09
unknown <jmb9974@GDD-30.main.ad.rit.edu>
2015-03-04 14:33:28 -0500

    Split PlayerSamurai into 2 scripts: Player and Samurai. That way, we can have samurai that are controlled by other things, namely AI.

---
+++ Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Samurai))]
+public class Player : MonoBehaviour {
+
+    Samurai _samurai;
+
+	// Use this for initialization
+	void Start () {
+        _samurai = GetComponent<Samurai>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Left mouse button pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!_samurai.hasTarget)
+                _samurai.Charge();
+        }
+
+        // TODO: Attacking, Dashing, and Interacting
+
+        if (_samurai.hasTarget)
+            transform.LookAt(_samurai.chargeTarget.transform);
+
+
+        switch (_samurai.CurrentBodyState)
+        {
+            case Samurai.BodyState.Charging:
+                transform.LookAt(_samurai.chargeTarget.transform);
+
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    _samurai.Attack();
+                    _samurai.lockOff();
+                }
+                break;
+            case Samurai.BodyState.Attacking:
+                transform.LookAt(_samurai.attackTarget);
+
+                GetComponent<CharacterController>().Move((_samurai.attackTarget - transform.position).normalized * Time.deltaTime * 50);
+                _samurai.attackTimer += Time.deltaTime;
+
+                if (Vector3.Distance(_samurai.attackTarget, transform.position) <= 5 || _samurai.attackTimer >= 5.0 || Input.GetKeyDown(KeyCode.T))
+                {
+                    _samurai.CurrentBodyState = Samurai.BodyState.Idle;
+                    _samurai.attackTarget = Vector3.zero;
+                    _samurai.attackTimer = 0;
+                }
+                break;
+            case Samurai.BodyState.Running:
+                break;
+            case Samurai.BodyState.Dashing:
+                break;
+            case Samurai.BodyState.Idle:
+                break;
+            default:
+                break;
+        }
+	}
+}
---
+++ Prototype/Assets/Scripts/Samurai.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Samurai : MonoBehaviour {
+
+    public int Might;
+    public int Honor;
+    public int Glory;
+    public int Cunning;
+    public int Wisdom;
+
+    public int Health;
+
+    public enum BodyState
+    {
+        Charging, Attacking, Running, Dashing, Idle
+    }
+
+    public GameObject chargeTarget;
+    public bool hasTarget;
+
+    public Vector3 attackTarget;
+
+    public BodyState CurrentBodyState;
+
+    public double attackTimer = 0.0;
+
+	// Use this for initialization
+	void Start () {
+        Health = 2;
+        Might = Honor = Glory = Cunning = Wisdom = 1;
+        CurrentBodyState = BodyState.Idle;
+        chargeTarget = null;
+        hasTarget = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Charge()
+    {
+        if (!hasTarget)
+            if (lockOn())
+                CurrentBodyState = BodyState.Charging;
+    }
+
+    public void Attack()
+    {
+        if (hasTarget)
+        {
+            attackTarget = chargeTarget.transform.position;
+            transform.LookAt(attackTarget);
+
+            CurrentBodyState = BodyState.Attacking;
+
+            chargeTarget = null;
+        }
+    }
+
+    public void Dash()
+    {
+
+    }
+
+    public void Interact()
+    {
+
+    }
+
+    public bool lockOn()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        {
+            chargeTarget = hit.collider.gameObject;
+            hasTarget = true;
+
+            CharacterMotor cm = GetComponent<CharacterMotor>();
+            cm.movement.maxForwardSpeed = 3;
+            cm.movement.maxBackwardsSpeed = 3;
+            cm.movement.maxSidewaysSpeed = 3;
+
+            return true;
+        }
+        return false;
+    }
+
+    public void lockOff()
+    {
+        chargeTarget = null;
+        hasTarget = false;
+
+        CharacterMotor cm = GetComponent<CharacterMotor>();
+        cm.movement.maxForwardSpeed = 10;
+        cm.movement.maxBackwardsSpeed = 10;
+        cm.movement.maxSidewaysSpeed = 10;
+
+        // add force/something to shoot player towards the target's location at the time of the attack
+        // ask Chris about this; apply force maybe, but currently no rigidbody on it, maybe easier way
+    }
+}
